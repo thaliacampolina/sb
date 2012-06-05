18bd235f25bb418d2f3e5ccd7af0ab017d8cbe1d
Thalia <thalia@thatha.(none)>
2012-06-05 18:41:27 -0300

    adding function heads

--- tp1_thaliacampolina/maquinavirtual.h
+++ tp1_thaliacampolina/maquinavirtual.h
@@ -1,9 +1,10 @@
+#ifndef _maquinavirtual_h
+#define _maquinavirtual_h
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-#ifndef _maquinavirtual_h
-#define _maquinavirtual_h
 
 typedef struct maquinavirtual{
     int RAM_[1000];
@@ -15,5 +16,55 @@ typedef struct maquinavirtual{
     int SP_;
 } MaquinaVirtual;
 
+//01
+MaquinaVirtual LOAD(MaquinaVirtual MV);
+//02
+MaquinaVirtual STORE(MaquinaVirtual MV);
+//03
+MaquinaVirtual PUSH(MaquinaVirtual MV);
+//04
+MaquinaVirtual POP(MaquinaVirtual MV);
+//05
+MaquinaVirtual JMP(MaquinaVirtual MV);
+//06
+// if AC_>0
+MaquinaVirtual JPG(MaquinaVirtual MV);
+//07
+// if AC_>=0
+MaquinaVirtual JPGE(MaquinaVirtual MV);
+//08
+// if AC_<0
+MaquinaVirtual JPL(MaquinaVirtual MV);
+//09
+// if AC_<=0
+MaquinaVirtual JPLE(MaquinaVirtual MV);
+//10
+// if AC_=0
+MaquinaVirtual JPE(MaquinaVirtual MV);
+//11
+// if AC_!=0
+MaquinaVirtual JPNE(MaquinaVirtual MV);
+//12
+MaquinaVirtual XOR(MaquinaVirtual MV);
+//13
+MaquinaVirtual AND(MaquinaVirtual MV);
+//14
+MaquinaVirtual OR(MaquinaVirtual MV);
+//15
+MaquinaVirtual NOT(MaquinaVirtual MV);
+//16
+MaquinaVirtual ADD(MaquinaVirtual MV);
+//17
+MaquinaVirtual SUB(MaquinaVirtual MV);
+//18
+MaquinaVirtual READ(MaquinaVirtual MV);
+//19
+MaquinaVirtual WRITE(MaquinaVirtual MV);
+//20
+MaquinaVirtual CALL(MaquinaVirtual MV);
+//21
+MaquinaVirtual RET(MaquinaVirtual MV);
+//22
+MaquinaVirtual HALT(MaquinaVirtual MV);
 
 #endif
