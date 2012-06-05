ae417fff5bf413872aa92e5207f355e86959641c
Thalia <thalia@thatha.(none)>
2012-06-05 18:46:58 -0300

    fixing indentation

--- tp1_thaliacampolina/main.c
+++ tp1_thaliacampolina/main.c
@@ -10,247 +10,244 @@ void executaOperacoes(MaquinaVirtual MV, int verbose){
     int instrucao=MV.RAM_[i];
 
     //INTRUCTION JMP
-     if(instrucao==1){
-            MV=JMP(MV);
-            if(verbose==1){
-                printf("Executando o JMP \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==1){
+        MV=JMP(MV);
+        if(verbose==1){
+            printf("Executando o JMP \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
     //INTRUCTION STORE
-     if(instrucao==2){
-            MV=STORE(MV);
-            if(verbose==1){
-                printf("Executando o STORE \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==2){
+        MV=STORE(MV);
+        if(verbose==1){
+            printf("Executando o STORE \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
     //INTRUCTION PUSH
-     if(instrucao==3){
-            MV=PUSH(MV);
-            if(verbose==1){
-                printf("Executando o PUSH \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==3){
+        MV=PUSH(MV);
+        if(verbose==1){
+            printf("Executando o PUSH \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
     //INTRUCTION POP
-     if(instrucao==4){
-            MV=POP(MV);
-            if(verbose==1){
-                printf("Executando o POP \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==4){
+        MV=POP(MV);
+        if(verbose==1){
+            printf("Executando o POP \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
     //INTRUCTION JMP
-     if(instrucao==5){
-            MV=JMP(MV);
-            if(verbose==1){
-                printf("Executando o JMP \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==5){
+        MV=JMP(MV);
+        if(verbose==1){
+            printf("Executando o JMP \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
     //INTRUCTION JPG
-     if(instrucao==6){
-            MV=JPG(MV);
-            if(verbose==1){
-                printf("Executando o JPG \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==6){
+        MV=JPG(MV);
+        if(verbose==1){
+            printf("Executando o JPG \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
     //INTRUCTION JPGE
-     if(instrucao==7){
-            MV=JPGE(MV);
-            if(verbose==1){
-                printf("Executando o JPGE \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==7){
+        MV=JPGE(MV);
+        if(verbose==1){
+            printf("Executando o JPGE \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
     //INTRUCTION JPL
-     if(instrucao==8){
-            MV=JPL(MV);
-            if(verbose==1){
-                printf("Executando o JPL \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==8){
+        MV=JPL(MV);
+        if(verbose==1){
+            printf("Executando o JPL \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
     //INTRUCTION JPLE
-     if(instrucao==9){
-            MV=JPLE(MV);
-            if(verbose==1){
-                printf("Executando o JPLE \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==9){
+        MV=JPLE(MV);
+        if(verbose==1){
+            printf("Executando o JPLE \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
     //INTRUCTION JPE
-     if(instrucao==10){
-            MV=JPE(MV);
-            if(verbose==1){
-                printf("Executando o JPE \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==10){
+        MV=JPE(MV);
+        if(verbose==1){
+            printf("Executando o JPE \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
     //INTRUCTION JPNE
-     if(instrucao==11){
-            MV=JPNE(MV);
-            if(verbose==1){
-                printf("Executando o JPNE --\n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==11){
+        MV=JPNE(MV);
+        if(verbose==1){
+            printf("Executando o JPNE --\n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
 
     //INTRUCTION XOR
-     if(instrucao==12){
-            MV=XOR(MV);
-            if(verbose==1){
-                printf("Executando o XOR \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==12){
+        MV=XOR(MV);
+        if(verbose==1){
+            printf("Executando o XOR \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
     //INTRUCTION AND
-     if(instrucao==13){
-            MV=AND(MV);
-            if(verbose==1){
-                printf("Executando o AND \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==13){
+        MV=AND(MV);
+        if(verbose==1){
+            printf("Executando o AND \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
 
     //INTRUCTION OR
-     if(instrucao==14){
-            MV=OR(MV);
-            if(verbose==1){
-                printf("Executando o OR --\n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==14){
+        MV=OR(MV);
+        if(verbose==1){
+            printf("Executando o OR \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
     //INTRUCTION NOT
-     if(instrucao==15){
-            MV=NOT(MV);
-            if(verbose==1){
-                printf("Executando o NOT \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==15){
+        MV=NOT(MV);
+        if(verbose==1){
+            printf("Executando o NOT \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
     //INTRUCTION ADD
-     if(instrucao==16){
-            MV=ADD(MV);
-            if(verbose==1){
-                printf("Executando o ADD \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==16){
+        MV=ADD(MV);
+        if(verbose==1){
+            printf("Executando o ADD \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
 
     //INTRUCTION SUB
-     if(instrucao==17){
-            MV=SUB(MV);
-            if(verbose==1){
-                printf("Executando o SUB \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==17){
+        MV=SUB(MV);
+        if(verbose==1){
+            printf("Executando o SUB \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
     //INTRUCTION READ
-     if(instrucao==18){
-            MV=READ(MV);
-            if(verbose==1){
-                printf("Executando o READ \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==18){
+        MV=READ(MV);
+        if(verbose==1){
+            printf("Executando o READ \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
 
     //INTRUCTION WRITE
-     if(instrucao==19){
-            MV=WRITE(MV);
-            if(verbose==1){
-                printf("Executando o WRITE \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==19){
+        MV=WRITE(MV);
+        if(verbose==1){
+            printf("Executando o WRITE \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
     //INTRUCTION CALL
-     if(instrucao==20){
-            MV=CALL(MV);
-            if(verbose==1){
-                printf("Executando o CALL \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==20){
+        MV=CALL(MV);
+        if(verbose==1){
+            printf("Executando o CALL \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
     //INTRUCTION RET
-     if(instrucao==21){
-            MV=RET(MV);
-            if(verbose==1){
-                printf("Executando o RET \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==21){
+        MV=RET(MV);
+        if(verbose==1){
+            printf("Executando o RET \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
+    }
 
     //INTRUCTION HALT
-     if(instrucao==22){
-            MV=HALT(MV);
-            if(verbose==1){
-                printf("Executando o HALT \n ");
-                printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
-                printf("\n ");
-            }
+    if(instrucao==22){
+        MV=HALT(MV);
+        if(verbose==1){
+            printf("Executando o HALT \n ");
+            printf("PC_=%d, SP_=%d, AC_=%d\n.",MV.PC_,MV.SP_,MV.AC_);
+            printf("\n ");
         }
-
-
+    }
 
 
 }
-
 
 
 
@@ -273,7 +270,7 @@ int main (int argc, char* argv[]) {
 //inicializa maquina virtual, memoria=0
         MV.PC_=atoi(argv[1]);
         MV.SP_=atoi(argv[2]);
-       MV.AC_=0;
+        MV.AC_=0;
         int i;
         for(i=0;i<1000;i++){
             MV.RAM_[i]=0;
